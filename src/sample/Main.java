6040146f624f413cb30a99b6484f434991d209b0
IsseNisse <isak.vindeln@gmail.com>
2021-03-23 14:10:14 +0100

    Skapat en metod för att skapa en xml-fil

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,14 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        createXml();
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setMaximized(true);
         primaryStage.show();
+    }
+
+    private void createXml() {
+        try {
+            File myObj = new File("filename.xml");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
 
