e04c9f8cb1a0c90d4bcfdb846a6a89cc9132f438
IsseNisse <isak.vindeln@gmail.com>
2021-03-23 14:51:56 +0100

    har gjort en metod för att skapa en linje med xml som svg

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Controller {
     @FXML
     private Canvas canvas;
@@ -30,6 +33,21 @@ public class Controller {
             gc.setStroke(strokeColor);
             gc.setLineWidth(size);
             gc.strokeLine(startX, startY, mouseX, mouseY);
+            createSVGPath(startX, startY, mouseX, mouseY, "line");
+        }
+    }
+
+    private void createSVGPath(double startX, double startY, double mouseX, double mouseY, String type) {
+        if (type.equals("line")) {
+            try {
+                FileWriter fileWriter = new FileWriter("filename.xml");
+                fileWriter.write(" <svg height=\"210\" width=\"500\">\n" +
+                        "  <line x1=\"" + startX +"\" y1=\"0\" x2=\"200\" y2=\"200\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />\n" +
+                        "</svg> ");
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
