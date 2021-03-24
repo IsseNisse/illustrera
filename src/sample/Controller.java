41dd9c713d7ab3c7845b980209abce379aeb1f71
IsseNisse <isak.vindeln@gmail.com>
2021-03-24 10:46:34 +0100

    har skapat en lista där alla saker som ska ritas ut finns och sedan en foreach för att rita ut

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,13 +6,19 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
     @FXML
     private Canvas canvas;
+
+    private ArrayList<Shape> shapes = new ArrayList<>();
 
     private final Color strokeColor = Color.BLACK;
     private int size = 20;
@@ -30,24 +36,32 @@ public class Controller {
                 startY = mouseY;
             }
         } else {
+            Line line = new Line(startX, startY, mouseX, mouseY);
+            shapes.add(line);
             gc.setStroke(strokeColor);
             gc.setLineWidth(size);
-            gc.strokeLine(startX, startY, mouseX, mouseY);
-            createSVGPath(startX, startY, mouseX, mouseY, "line");
+            for (Shape shape : shapes) {
+                String type = shape.getTypeSelector();
+                if (type.equals("Line")) {
+                    Line line2 = (Line)shape;
+                    gc.strokeLine(line2.getStartX(), line2.getStartY(), line2.getEndX(), line2.getEndY());
+                }
+            }
+//            createSVGPath(startX, startY, mouseX, mouseY, "line");
         }
     }
 
-    private void createSVGPath(double startX, double startY, double mouseX, double mouseY, String type) {
-        if (type.equals("line")) {
-            try {
-                FileWriter fileWriter = new FileWriter("filename.xml");
-                fileWriter.write(" <svg height=\"210\" width=\"500\">\n" +
-                        "  <line x1=\"" + startX +"\" y1=\"0\" x2=\"200\" y2=\"200\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />\n" +
-                        "</svg> ");
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    private void createSVGPath(double startX, double startY, double mouseX, double mouseY, String type) {
+//        if (type.equals("line")) {
+//            try {
+//                FileWriter fileWriter = new FileWriter("filename.xml");
+//                fileWriter.write(" <svg height=\"210\" width=\"500\">\n" +
+//                        "  <line x1=\"" + startX +"\" y1=\"0\" x2=\"200\" y2=\"200\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />\n" +
+//                        "</svg> ");
+//                fileWriter.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
