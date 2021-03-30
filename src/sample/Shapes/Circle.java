0868722f55e6229c0169dc45d193f5f558fef57d
IsseNisse <isak.vindeln@gmail.com>
2021-03-30 13:46:01 +0200

    har gjort en cirkel class

---
+++ src/sample/Shapes/Circle.java
@@ -0,0 +1,25 @@
+package sample.Shapes;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Circle extends Shape {
+    public Circle(double startX, double startY, double endX, double endY) {
+        super(startX, startY, endX, endY);
+    }
+
+    public void calculateArea() {
+        double radius1 = (getEndX() - getStartX())/2;
+        double radius2 = (getEndY() - getStartY())/2;
+
+        double area = Math.PI * radius1 * radius2;
+        super.setArea(area);
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.setStroke(getStroke());
+        gc.setFill(getFill());
+        gc.setLineWidth(getSize());
+        gc.fillOval(getStartX(), getStartY(), getEndX(), getEndY());
+        gc.strokeOval(getStartX(), getStartY(), getEndX(), getEndY());
+    }
+}
