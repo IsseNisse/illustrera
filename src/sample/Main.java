08ab86e42d8bdfcdae0a9b01327d1aea1ea993ba
IsseNisse <isak.vindeln@gmail.com>
2021-04-20 14:00:55 +0200

    har lagt till och kopplat en css fil till fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Illustrera");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 900, 575));
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
